a70179514a65f5c03de5439d3b790de06f70ede7
Benoît <benoitobelia@gmail.com>
2023-05-14 16:05:54 +0200

    Ajout de routes sur Flask
    /humidity
    /temperature
    /carbone

--- site/main.py
+++ site/main.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
+@app.route('/humidity')
+def humidity():
+    return render_template('humidite.html')
+
+@app.route('/temperature')
+def temperature():
+    return render_template('temperature.html')
+
+@app.route('/carbone')
+def carbone():
+    return render_template('carbone.html')
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
