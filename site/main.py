e41db96ba58881894dbe03e80d9b2628b799edab
Benoît <benoitobelia@gmail.com>
2023-05-14 17:04:57 +0200

    Ajout des données dans Flash (render_template)
    Données retournée dans {{ data }} via un tableau à 2 entrées pour humidité et température, à 3 entrées pour vent

--- site/main.py
+++ site/main.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template
+import sqlite3
+
+name_db = "data/test1.db"
+
+db = sqlite3.connect(name_db, check_same_thread=False)
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS mesures(time INTEGER,temperature FLOAT, humidite FLOAT, force_vent FLOAT, direction_vent FLOAT)") # création de la table si elle existe pas
+
 
 app = Flask(__name__)
 
@@ -6,19 +14,23 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
-@app.route('/humidity')
+@app.route('/humidite')
 def humidity():
-    return render_template('humidite.html')
+    data_humidite = cur.execute("SELECT time,humidite FROM mesures").fetchall()
+    data = [[x[0], x[1]] for x in data_humidite]
+    return render_template('humidite.html', data_humidite=data, title="Humidité")
 
 @app.route('/temperature')
 def temperature():
-    return render_template('temperature.html')
+    data_temperature = cur.execute("SELECT time,temperature FROM mesures").fetchall()
+    data = [[x[0], x[1]] for x in data_temperature]
+    return render_template('temperature.html', data_temperature=data, title="Température")
 
-@app.route('/carbone')
-def carbone():
-    return render_template('carbone.html')
+@app.route('/vent')
+def vent():
+    data_vent = cur.execute("SELECT time,force_vent, direction_vent FROM mesures").fetchall()
+    data = [[x[0], x[1], x[2]] for x in data_vent]
+    return render_template('vent.html', data_vent=data, title="Vent")
 
 
-if __name__ == '__main__':
-
-    app.run(debug=True)
+app.run(debug=True)
