ef30cf902143020939de1ed20375ea7e4f663561
paulemile-hash <133578402+paulemile-hash@users.noreply.github.com>
2023-06-22 19:19:41 +0200

    convertiseur

--- tools/Convertiseur.py
+++ tools/Convertiseur.py
@@ -1,4 +1,7 @@
 import serial
+import sqlite3
+import time
+import serial
 
 port = 'COM7'
 baud_rate = 9600
@@ -19,7 +22,7 @@ try:
                     data = ser.read(ser.in_waiting).decode('utf-8')
                     
                     # Écrire les données dans le fichier de sortie
-                    file.write(data)
+                    file.write(data.strip())
                     
                     # Afficher les données à la console
                     print(data, end='')
@@ -31,3 +34,30 @@ except IOError as e:
     exit(1)
 
 ser.close()
+
+
+
+name_db = "data/test1.db"
+db = sqlite3.connect(name_db)
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS mesures(time FLOAT,temperature FLOAT, humidite FLOAT, force_vent FLOAT, direction_vent FLOAT)") # création de la table si elle existe pas
+
+
+
+def lecture (txt):
+    fichier=open(txt,"r")
+    for row in fichier:
+        x=row.split()# créer une liste dés qu'il y a un espace
+        x.insert(0, time.time())
+        cur.execute(f"""INSERT INTO mesures VALUES
+        ({x[0]},{x[1]},{x[2]},{x[3]},{x[4]})  
+        """)
+         #transferer mes valeurs de data.txt dans la base de données
+    fichier.close()
+
+
+
+lecture("data.txt")
+db.commit()
+db.close()
+
