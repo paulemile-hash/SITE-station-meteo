b98c3133a1f44671500544e7ec2353ab2234696a
paulemile-hash <133578402+paulemile-hash@users.noreply.github.com>
2023-06-22 21:47:21 +0200

    transmission site

--- tools/Convertiseur.py
+++ tools/Convertiseur.py
@@ -1,11 +1,12 @@
 import serial
 import sqlite3
 import time
-import serial
 
 port = 'COM7'
 baud_rate = 9600
-output_file = 'data.txt'
+output_file = 'data/data.txt'
+cpt=0
+
 
 try:
     ser = serial.Serial(port, baud_rate)
@@ -15,17 +16,17 @@ except serial.SerialException as e:
 
 try:
     with open(output_file, 'w') as file:
-        while True:
+        while cpt<5 :
             try:
                 if ser.in_waiting > 0:
                     # Lire les données disponibles dans le buffer de réception
                     data = ser.read(ser.in_waiting).decode('utf-8')
-                    
-                    # Écrire les données dans le fichier de sortie
-                    file.write(data.strip())
-                    
-                    # Afficher les données à la console
+                     
+                    cpt+=1
                     print(data, end='')
+                    # Écrire les données dans le fichier de sortie
+                    file.write(data)
+                    
             except serial.SerialException as e:
                 print(f"Erreur lors de la lecture du port série : {e}")
                 break
@@ -40,24 +41,28 @@ ser.close()
 name_db = "data/test1.db"
 db = sqlite3.connect(name_db)
 cur = db.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS mesures(time FLOAT,temperature FLOAT, humidite FLOAT, force_vent FLOAT, direction_vent FLOAT)") # création de la table si elle existe pas
-
+cur.execute("CREATE TABLE IF NOT EXISTS mesures(time FLOAT,temperature FLOAT, humidite FLOAT, force_vent FLOAT, direction_vent VARCHAR(1))") # création de la table si elle existe pas
+db.commit()
 
 
 def lecture (txt):
     fichier=open(txt,"r")
     for row in fichier:
-        x=row.split()# créer une liste dés qu'il y a un espace
-        x.insert(0, time.time())
-        cur.execute(f"""INSERT INTO mesures VALUES
-        ({x[0]},{x[1]},{x[2]},{x[3]},{x[4]})  
-        """)
+        if row =="":
+            pass
+        else:
+            x=row.split()# créer une liste dés qu'il y a un espace
+            x.insert(0, time.time())
+            if len(x) == 5:
+                x=tuple(x)
+                print(x)
+                cur.execute("INSERT INTO mesures (time, temperature, humidite, force_vent, direction_vent) VALUES (?, ?, ?, ?, ?)", x)
+                db.commit()
          #transferer mes valeurs de data.txt dans la base de données
     fichier.close()
 
 
 
-lecture("data.txt")
-db.commit()
+lecture(output_file)
 db.close()
 
